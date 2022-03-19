2c375781f6de8f49535302bbd9031c7ec9176431
MkMonir <khanmonir1335@gmail.com>
2022-03-20 01:11:37 +0600

    Create server using express and connect mongodb database

---
+++ server.js
@@ -0,0 +1,22 @@
+const dotenv = require('dotenv');
+const mongoose = require('mongoose');
+
+dotenv.config({ path: './config.env' });
+const app = require('./app');
+
+const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.DATABASE_PASSWORD);
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log('Database successfully connected'));
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => {
+  console.log(`App successfully running on port ${port}`);
+});
