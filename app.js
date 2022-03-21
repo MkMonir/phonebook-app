5ecc0e0673f693ffa44c69eef3e9b32c4cd48be9
MkMonir <khanmonir1335@gmail.com>
2022-03-22 00:22:59 +0600

    Adding global error handling middleware to phonebook app

--- app.js
+++ app.js
@@ -1,14 +1,23 @@
 const express = require('express');
+const morgan = require('morgan');
 const contactRouter = require('./routes/contactRoutes');
+const userRouter = require('./routes/userRoutes');
+const globalErrorHandler = require('./controllers/errorController');
 
 const app = express();
 app.use(express.json());
 
-app.use((req, res, next) => {
-  req.requestTime = new Date().toISOString();
-  next();
-});
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
 
 app.use('/api/v1/contacts', contactRouter);
+app.use('/api/v1/users', userRouter);
+
+// note: GLOBAL ERROR HANDLING MIDDLEWARE
+app.all('*', (req, res, next) => {
+  next(new AppError(`Can't find ${req.originalUrl} on this server`, 404));
+});
+app.use(globalErrorHandler);
 
 module.exports = app;
