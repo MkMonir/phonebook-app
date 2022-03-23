edaac568a951ac8059864e78e32dfc0d61896987
MkMonir <khanmonir1335@gmail.com>
2022-03-24 00:23:08 +0600

    Clean repeated codes and add factory functionility

--- controllers/contactController.js
+++ controllers/contactController.js
@@ -1,55 +1,12 @@
 const Contact = require('./../models/contactModel');
-const catchAsync = require('./../utils/catchAsync');
+const factory = require('./handlerFactory');
 
-exports.getAllContacts = catchAsync(async function (req, res, next) {
-  const contacts = await Contact.find();
+exports.getAllContacts = factory.getAll(Contact);
 
-  res.status(200).json({
-    status: 'success',
-    results: contacts.length,
-    data: contacts,
-  });
-});
+exports.createContact = factory.createOne(Contact);
 
-exports.createContact = catchAsync(async function (req, res, next) {
-  const newContacts = await Contact.create(req.body);
+exports.getContactById = factory.getOne(Contact);
 
-  res.status(201).json({
-    status: 'success',
-    data: {
-      contact: newContacts,
-    },
-  });
-});
+exports.updateContact = factory.updateOne(Contact);
 
-exports.getContactById = catchAsync(async function (req, res, next) {
-  const contact = await Contact.findById(req.params.id);
-
-  res.status(200).json({
-    status: 'success',
-    data: {
-      contact,
-    },
-  });
-});
-
-exports.updateContact = catchAsync(async function (req, res, next) {
-  const contact = await Contact.findByIdAndUpdate(req.params.id, req.body, {
-    new: true,
-    runValidators: true,
-  });
-
-  res.status(200).json({
-    status: 'success',
-    data: { contact },
-  });
-});
-
-exports.deleteContact = catchAsync(async function (req, res, next) {
-  await Contact.delete(req.params.id);
-
-  res.status(204).json({
-    status: 'success',
-    data: null,
-  });
-});
+exports.deleteContact = factory.deleteOne(Contact);
