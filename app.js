ac6feedb60480a14ed71b0257f908c9b107f5105
MkMonir <khanmonir1335@gmail.com>
2022-03-28 23:15:17 +0600

    Add country phone code router in phonebook app

--- app.js
+++ app.js
@@ -2,10 +2,11 @@ const express = require('express');
 const morgan = require('morgan');
 const contactRouter = require('./routes/contactRoutes');
 const userRouter = require('./routes/userRoutes');
+const phoneCodeRouter = require('./routes/phoneCodeRoutes');
 const globalErrorHandler = require('./controllers/errorController');
 
 const app = express();
-app.use(express.json());
+app.use(express.json({ limit: '10kb' }));
 
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
@@ -13,6 +14,7 @@ if (process.env.NODE_ENV === 'development') {
 
 app.use('/api/v1/contacts', contactRouter);
 app.use('/api/v1/users', userRouter);
+app.use('/api/v1/phone-codes', phoneCodeRouter);
 
 // note: GLOBAL ERROR HANDLING MIDDLEWARE
 app.all('*', (req, res, next) => {
